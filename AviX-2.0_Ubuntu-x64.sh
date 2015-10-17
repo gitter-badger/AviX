94c1357c748115e9dd902e8ecd5fcb0e37acd963
Raul Dipeas <rauldipeas@outlook.com>
2015-10-17 08:16:13 -0300

    / ‘AviX-2.0_Ubuntu-x64.sh’

--- AviX-2.0_Ubuntu-x64.sh
+++ AviX-2.0_Ubuntu-x64.sh
@@ -620,6 +620,12 @@ sudo rm -v /usr/share/unity-greeter/cof.png
 cd ~/Downloads
 wget -c https://github.com/rauldipeas/AviX/raw/master/data/images_tango.zip
 sudo cp -v ~/Downloads/images_tango.zip /usr/share/libreoffice/share/config/
+
+##############################################################################################################################################################################################################################################################
+# Pidgin Numix Theme
+cd ~/Downloads
+wget -c
+sudo tar -vzxf pidgin.tar.gz -C /usr/share/pixmaps/
 ##############################################################################################################################################################################################################################################################
 # Vertex Firefox Theme
 killall -9 firefox
